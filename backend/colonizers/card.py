d4273a7c947fdb37737532292aa8905a3a7f0a4b
David Bao <davidbao@berkeley.edu>
2020-04-21 19:48:48 -0700

    basic set up for card classes

--- backend/colonizers/card.py
+++ backend/colonizers/card.py
@@ -1,3 +1,39 @@
 """
 Module for Card, MatCard, DevCard, etc.
 """
+
+class Card:
+    def __init__(self, category):
+        self.category = category //MatCard or DevCard
+
+class MatCard(Card):
+    def __init__(self, category, material):
+        super().__init__(category)
+        self.material = material
+
+class DevCard(Card):
+    def __init__(self, category):
+        super().__init__(category)
+        _used = False
+
+class Knight(DevCard):
+    def __init__(self, category):
+        super().__init__(category)
+
+class YearOfPlentyDevCard):
+    def __init__(self, category):
+        super().__init__(category)
+
+class TwoRoads(DevCard):
+    def __init__(self, category):
+        super().__init__(category)
+
+class Monopoly(DevCard):
+    def __init__(self, category):
+        super().__init__(category)
+
+class VPCard(DevCard):
+    def __init__(self, category):
+        super().__init__(category)
+    def use():
+        Player.add_vp(1)
