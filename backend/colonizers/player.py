d9494c19afd56cb27f73bb6009f5b06438ee6660
Vincent Ngo <vincent.ngo31@berkeley.edu>
2020-04-22 00:55:36 -0700

    setup  Game and Player classes

--- backend/colonizers/player.py
+++ backend/colonizers/player.py
@@ -1,3 +1,16 @@
 """
 Module for Player, Building, etc.
 """
+import card
+
+class Player:
+
+    def __init__(materials):
+        self._materials = {'wood': 0, 'sheep': 0, 'brick': 0, 'ore': 0, 'wheat': 0}
+        self._buildings: {'roads': 0, 'settlements': 0, 'cities': 0}
+        self._devcards = []
+        self._color = None
+
+    def change_mats(mats, ct):
+        # Takes in list of materials and corresponding list of counts and changes player's materials
+        self._materials[m] += ct for m in mats
