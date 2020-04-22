3b7bd1abcebdc3bd1e5978b0eafed378dcea40da
Drake Wong <drakejwong@gmail.com>
2020-04-22 09:36:02 -0700

    Revise turn system, attrs

--- backend/colonizers/game.py
+++ backend/colonizers/game.py
@@ -1,50 +1,52 @@
 """
 Module for Game
 """
-import player, board, card
+from player import Player, Building
+import board
+import card
 import random
-from collections import OrderedDict
+
 
 class Game():
 
     def __init__(self, num_players):
-        self._players = [player.Player() for n in num_players]
-        self._winner = None
-        self._current_turn = 0
-        self._longest_road = (None, 0)
-        self._largest_army = (None, 0)
-
-    def next_turn():
-        # Updates turn 
-        self._current_turn = (self._current_turn + 1) % len(self._players)
+        self._players = [Player() for n in num_players]
+        self._turn = 0
+        self._longest_road = [None, 0]
+        self._largest_army = [None, 0]
     
+    @property
+    def curr(self):
+        return self._players[self._turn]
+
+    def next_turn(self):
+        self._turn = (self._turn + 1) % len(self._players)
+
     def roll():
         die1 = random.randint(1, 6)
         die2 = random.randint(1, 6)
-        self._curr_roll = die1 + die2
-        self.harvest()
+        self.harvest(die1 + die2)
 
-    def harvest():
+    def harvest(self, roll):
+        # for all hexes, for all buildings, house.player.transact
+
+    def build_settlement(self):
         pass
 
-    def build_settlement():
+    def build_road(self):
         pass
 
-    def build_road():
+    def build_dev_card(self):
         pass
 
-    def build_dev_card():
+    def upgrade_settlement(self):
         pass
 
-    def upgrade_settlement():
+    def trade(self):
         pass
 
-    def trade():
+    def use_dev_card(self):
         pass
 
-    def use_dev_card():
+    def move_robber(self):
         pass
-
-    def move_robber():
-        pass
-    
