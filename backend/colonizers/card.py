3db803e5b45e0d08f6eb78429da83ae0b69894e5
David Bao <davidbao@berkeley.edu>
2020-04-22 11:59:09 -0700

    removed matcard/card classes. reorganized devcard class. updated init and use methods accordingly

--- backend/colonizers/card.py
+++ backend/colonizers/card.py
@@ -1,10 +1,14 @@
 """
 Module for Card, MatCard, DevCard, etc.
 """
-//do I need to import the other modules?
-//made the use() method for dev cards have Player as a param
-
 import player
+"""
+player.transact("wood", 2)
+    --> adds 2 wood to that player's mats
+player.transact(["wood", "stone"], [1, -3])
+    --> +1 wood, -3 stone for a player. (3:1 port functionality)
+"""
+
 
 __all__ = [
     "Knight",
@@ -13,61 +17,55 @@ __all__ = [
     "Monopoly",
     "VPCard",
 ]
-
-
-class Card:
-    def __init__(self, category):
-        self.category = category // MatCard or DevCard
-
-
-class MatCard(Card):
-    def __init__(self, category, material):
-        super().__init__(category)
-        self.material = material
-
-
-class DevCard(Card):
-    def __init__(self, category):
-        super().__init__(category)
-        _used = False
-
+class DevCard(object):
+    def __init__(self):
+        self._used = False
 
 class Knight(DevCard):
-    def __init__(self, category):
-        super().__init__(category)
-    def use(Player):
-        //Should Game handle move_robber? spec says owner.move_robber
-        move_robber(Player, Hex)
-        _used = True
+    def __init__(self):
+        super().__init__()
+        self.description = "Move the robber." \
+                           "Steal 1 resource from the owner of a settlement or city" \
+                           "adjacent to the robber's new hex"
+    def use(self, self, player):
+        #Should Game handle move_robber? spec says owner.move_robber
+        # move_robber(player, Hex)
+        self._used = True
 
-class YearOfPlentyDevCard):
-    def __init__(self, category):
-        super().__init__(category)
-    def use(Player, mat1, mat2):
-        Player.materials[mat1] += 1
-        Player.materials[mat2] += 2
-        _used = True
+class YearOfPlentyDevCard(DevCard):
+    def __init__(self):
+        super().__init__()
+        self.description = "Take any two resources from the bank. Add them to your hand." \
+                           "Can be 2 of the same resource or 2 different resources."
+    def use(self, player, mat1, mat2, amt1, amt2):
+        player.transact([mat1, mat2], [amt1, amt2])
+        self._used = True
 
 class TwoRoads(DevCard):
-    def __init__(self, category):
-        super().__init__(category)
-    def use(Player):
-        //spec says build_road(List[Hex])
-        build_road(Player, Start, End)
-        _used = True
+    def __init__(self):
+        self.super().__init__()
+        self.description = "Place 2 new roads as if you had just built them."
+    def use(self, player):
+        #spec says build_road(List[Hex])
+        # Game will handle
+        # build_road(player, Start, End)
+        self._used = True
 
 class Monopoly(DevCard):
-    def __init__(self, category):
-        super().__init__(category)
-    def use(mat_type_str, Player):
-        //no sure how we are handling trade
-        //??
-        Player.trade(Other_player, P_input, O_input, P_amt, O_amt)
-        _used = True
+    def __init__(self):
+        super().__init__()
+        self.description = "When you play this card, announce 1 type of resource." \
+                           "All players must give you all of their resources of that type."
+    def use(self, mat_type_str, player):
+        #no sure how we are handling trade
+        #??
+        player.trade(Other_player, [], mat_type_str, P_amt, O_amt)
+        self._used = True
 
 class VPCard(DevCard):
-    def __init__(self, category):
-        super().__init__(category)
-    def use(Player):
-        Player.add_vp(1) //do we want to show this? Points from VP cards are hidden until end
-        _used = True
+    def __init__(self):
+        super().__init__()
+        self.description = "1 Victory Point!"
+    def use(self, player):
+        player.add_vp(1) #do we want to show this? Points from VP cards are hidden until end
+        self._used = True
